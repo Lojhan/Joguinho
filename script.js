20b81b7a9fba715493b08b2601edb57b0916532b
Vinícius Lojhan Martinez Hernandes <47763156+Lojhan@users.noreply.github.com>
2020-05-27 22:50:11 -0300

    sla

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
 const draggableElements = document.querySelectorAll(".draggable");
 const droppableElements = document.querySelectorAll(".droppable");
-let pontuacao = 0;
-
-
-
-
-
-
-let sapo = "frog";
-let pontosSapo = 3;
-
-let aranha = "spider";
-let pontosAranha = 2;
-
-
-
-
-
-
+let pontuacao = [7];
 
 draggableElements.forEach(elem => {
-  elem.addEventListener("dragstart", dragStart); // Fires as soon as the user starts dragging an item - This is where we can define the drag data
-  // elem.addEventListener("drag", drag); // Fires when a dragged item (element or text selection) is dragged
-  // elem.addEventListener("dragend", dragEnd); // Fires when a drag operation ends (such as releasing a mouse button or hitting the Esc key) - After the dragend event, the drag and drop operation is complete
+  elem.addEventListener("dragstart", dragStart);  
 });
+
 
 droppableElements.forEach(elem => {
-  elem.addEventListener("dragenter", dragEnter); // Fires when a dragged item enters a valid drop target
-  elem.addEventListener("dragover", dragOver); // Fires when a dragged item is being dragged over a valid drop target, repeatedly while the draggable item is within the drop zone
-  elem.addEventListener("dragleave", dragLeave); // Fires when a dragged item leaves a valid drop target
-  elem.addEventListener("drop", drop); // Fires when an item is dropped on a valid drop target
+  elem.addEventListener("dragenter", dragEnter); 
+  elem.addEventListener("dragover", dragOver); 
+  elem.addEventListener("dragleave", dragLeave); 
+  elem.addEventListener("drop", drop); 
+
+  
+
 });
 
-// Drag and Drop Functions
-
-//Events fired on the drag target
-
 function dragStart(event) {
-  event.dataTransfer.setData("text", event.target.id); // or "text/plain" but just "text" would also be fine since we are not setting any other type/format for data value
+  event.dataTransfer.setData("text", event.target.id); 
+  
 }
-
-
-
-//Events fired on the drop target
 
 function dragEnter(event) {
   if(!event.target.classList.contains("dropped")) {
@@ -50,9 +28,11 @@ function dragEnter(event) {
   }
 }
 
+
+
 function dragOver(event) {
   if(!event.target.classList.contains("dropped")) {
-    event.preventDefault(); // Prevent default to allow drop
+    event.preventDefault(); 
   }
 }
 
@@ -63,38 +43,32 @@ function dragLeave(event) {
 }
 
 function drop(event) {
-  event.preventDefault(); // This is in order to prevent the browser default handling of the data
+
+
+  event.preventDefault(); 
   event.target.classList.remove("droppable-hover");
-  const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+  $("img").attr("game", pontuacao);
+
+  const draggableElementData = event.dataTransfer.getData("text");
   const droppableElementData = event.target.getAttribute("data-draggable-id");
   const isCorrectMatching = draggableElementData === droppableElementData;
 
 
-  
   if(isCorrectMatching) {
     const draggableElement = document.getElementById(draggableElementData);
 
-    if(draggableElementData == sapo){
-      pontuacao = pontuacao + pontosSapo;
-    }
+    var inGame = event.target.getAttribute("game");
 
-    if(draggableElementData == aranha){
-      pontuacao = pontuacao + pontosAranha;
-    }
-
-
-    
-
+    parseInt ("inGame", 10);
+    adicionaPontuacao = inGame;
 
 
     document.getElementById("result").innerHTML = ('Pontuação: ' + pontuacao);
 
-
-
-    // event.target.style.backgroundColor = draggableElement.style.color; // This approach works only for inline styles. A more general approach would be the following: 
     draggableElement.classList.add("dragged");
     draggableElement.setAttribute("draggable", "false");
     event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
+    // $("img").attr("game", "0");
 
     
 
