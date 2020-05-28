69cc47d6d2b7557cc898553e249dfb2715245168
Vinícius Lojhan Martinez Hernandes <47763156+Lojhan@users.noreply.github.com>
2020-05-27 23:58:10 -0300

    agora alguma coisa funciona

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const draggableElements = document.querySelectorAll(".draggable");
 const droppableElements = document.querySelectorAll(".droppable");
-let pontuacao = [7];
+let pontuacao = 0;
 
 draggableElements.forEach(elem => {
   elem.addEventListener("dragstart", dragStart);  
 });
-
 
 droppableElements.forEach(elem => {
   elem.addEventListener("dragenter", dragEnter); 
@@ -13,13 +12,12 @@ droppableElements.forEach(elem => {
   elem.addEventListener("dragleave", dragLeave); 
   elem.addEventListener("drop", drop); 
 
-  
-
 });
 
 function dragStart(event) {
   event.dataTransfer.setData("text", event.target.id); 
-  
+  pontosDoObj = event.currentTarget.attributes.game.nodeValue;
+  console.log(pontosDoObj);
 }
 
 function dragEnter(event) {
@@ -27,8 +25,6 @@ function dragEnter(event) {
     event.target.classList.add("droppable-hover");
   }
 }
-
-
 
 function dragOver(event) {
   if(!event.target.classList.contains("dropped")) {
@@ -44,30 +40,29 @@ function dragLeave(event) {
 
 function drop(event) {
 
-
   event.preventDefault(); 
   event.target.classList.remove("droppable-hover");
-  $("img").attr("game", pontuacao);
+  $("img").attr("game", pontosDoObj);
 
   const draggableElementData = event.dataTransfer.getData("text");
   const droppableElementData = event.target.getAttribute("data-draggable-id");
   const isCorrectMatching = draggableElementData === droppableElementData;
-
 
   if(isCorrectMatching) {
     const draggableElement = document.getElementById(draggableElementData);
 
     var inGame = event.target.getAttribute("game");
 
-    parseInt ("inGame", 10);
-    adicionaPontuacao = inGame;
+    issoAquiExisteSoPeloParse = parseInt(inGame, 10);
 
+    pontuacao = (pontuacao + issoAquiExisteSoPeloParse);
 
     document.getElementById("result").innerHTML = ('Pontuação: ' + pontuacao);
 
     draggableElement.classList.add("dragged");
     draggableElement.setAttribute("draggable", "false");
     event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
+  
     // $("img").attr("game", "0");
 
     
