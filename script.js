2eb6f8ce85767c5bfa778d336c62109942063be8
Vinícius Lojhan Martinez Hernandes <47763156+Lojhan@users.noreply.github.com>
2020-05-28 15:45:40 -0300

    agr ta melhor

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ droppableElements.forEach(elem => {
 function dragStart(event) {
   event.dataTransfer.setData("text", event.target.id); 
   pontosDoObj = event.currentTarget.attributes.game.nodeValue;
-  console.log(pontosDoObj);
+  valorUnico = event.currentTarget.attributes.unique.nodeValue;
+
+  console.log(valorUnico);
 }
 
 function dragEnter(event) {
@@ -49,31 +51,22 @@ function drop(event) {
   const isCorrectMatching = draggableElementData === droppableElementData;
 
   if(isCorrectMatching) {
+    
     const draggableElement = document.getElementById(draggableElementData);
 
     var inGame = event.target.getAttribute("game");
 
     issoAquiExisteSoPeloParse = parseInt(inGame, 10);
 
+    novoAtributo = $( "[unique=" + valorUnico + "]");
+
+    novoAtributo[0].setAttribute("draggable", "false");
+    novoAtributo[0].classList.add("dragged");
+  
     pontuacao = (pontuacao + issoAquiExisteSoPeloParse);
 
     document.getElementById("result").innerHTML = ('Pontuação: ' + pontuacao);
 
-    draggableElement.classList.add("dragged");
-    draggableElement.setAttribute("draggable", "false");
-    event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
-  
-    // $("img").attr("game", "0");
-
-    
-
   }
 }
 
-if (pontuacao == 1){
-
-  function delete_row(e){ 
-    e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode.parentNode.parentNode.parentNode);
-
-}
-}
